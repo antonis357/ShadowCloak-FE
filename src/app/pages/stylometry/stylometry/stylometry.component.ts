b195ccf3d9ecc8b57c8706a56dac2a26b8b57566
antonis <antonis.cyberlogic.gr@outlook.com>
2019-11-06 09:22:06 +0200

    Copy texts functionality

--- src/app/pages/stylometry/stylometry/stylometry.component.ts
+++ src/app/pages/stylometry/stylometry/stylometry.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stylometry.component.scss']
 })
 export class StylometryComponent implements OnInit {
+  // tslint:disable-next-line:max-line-length
+  anonymizedText = '';
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit() {
+  copyTextToAnonymousTextArea(userText: string) {
+    this.anonymizedText = userText;
   }
 
+  copyAnonymousTextToClipboard() {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.anonymizedText;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
 }
